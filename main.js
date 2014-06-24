f46612760782dfc08cac13fb3a9b928461db2355
Erik Shandrowski <erik843@gmail.com>
2014-06-24 12:32:43 -0600

    Update - getting there

--- main.js
+++ main.js
@@ -1,39 +1,37 @@
-var victims = [];
-var volunteers = [];
-var numberOfVictims = parseInt(prompt("How many victims are there to help?"));
-
-var addPeopleToList = function(listName, totalInList) {
-	for(i=0; i<totalInList; i++) {
-	var newName = prompt("Name number " + (i + 1) + "?")
-	listName.push({ name: newName });
-	listName[i]['tel'] = prompt("Enter number " + (i + 1) + "'s phone number:");
-	listName[i]['street'] = prompt("Enter number " + (i + 1) + "'s street name:");
-	return listName
-};
-
 function startDisasterRelief() {
-	// for(i=0; i<numberOfVictims; i++) {
-	// 	var victim = prompt("Name of victim " + (i + 1) + "?")
-	// 	victims.push({ name: victim });
-	// 	victims[i]['tel'] = prompt("Enter " + victim + "'s phone number:");
-	// 	victims[i]['street'] = prompt("Enter " + victim + "'s street name:");		
-	// }
+	// Adds people to list - takes in the list name and total to be added
+	// and outputs the updated list with each person as an object
+	var addPeopleToList = function(totalInList) {
+		var newArray = []
+		for(i=0; i<totalInList; i++) {
+			var newPerson = { 
+				name: prompt("Name number " + (i + 1) + "?"),
+				tel: prompt("Enter number " + (i + 1) + "'s phone number:"),
+				street: prompt("Enter number " + (i + 1) + "'s street name:")
+			}
+			newArray.push(newPerson);
+		}
+		return newArray;
+	};
 
-	var numberOfVolunteers = parseInt(prompt("How many volunteers are there?"));
+	//Add people to the victims list
+	var numberOfVictims = parseInt(prompt("How many victims are there to help?"));
+	var victims = addPeopleToList(numberOfVictims);
+	console.log(victims)
+	
+	//Add people to the volunteers list
+	var numberOfVolunteers = +prompt("How many volunteers are there?");
+	var volunteers = addPeopleToList(numberOfVolunteers);
+	console.log(volunteers);
 
-	for(i=0; i<numberOfVolunteers; i++) {
-		var volunteer = prompt("Name of volunteer " + (i + 1) + "?");
-		volunteers.push({ name: volunteer });
-		volunteers[i]['tel'] = prompt("Enter " + volunteer + "'s phone number:");
-		volunteers[i]['street'] = prompt("Enter " + volunteer + "'s street name:");
-	}	
-	// console.log(volunteers[0]);
-	// console.log(volunteers[1]);
-	// console.log(volunteers);
+	
 
-	alert("Thanks for helping us start Disaster Relief.\n\nThere are " 
-		+ numberOfVictims + " people who need help. \nAnd, " + numberOfVolunteers + 
-		" volunteers who can help.\n\nVictims:\n\t" + victims[0] + 
-		"\n\nVolunteers:\n\t" + volunteers);
+
+	// alert("Thanks for helping us start Disaster Relief.\n\nThere are " 
+	// 	+ numberOfVictims + " people who need help. \nAnd, " + numberOfVolunteers + 
+	// 	" volunteers who can help.\n\nVictims:\n\t" + victims[0] + 
+	// 	"\n\nVolunteers:\n\t" + volunteers);
+
+
 };
 startDisasterRelief();
