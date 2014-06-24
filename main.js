d23753ebcd265f785dc8338d76942d368020032d
Erik Shandrowski <erik843@gmail.com>
2014-06-24 14:54:04 -0600

    Bonus 1 finished

--- main.js
+++ main.js
@@ -1,26 +1,70 @@
+// function startDisasterRelief() {
+// 	// Adds people to list - takes in the list name and total to be added
+// 	// and outputs the updated list with each person as an object
+// 	var addPeopleToList = function(totalInList) {
+// 		var newArray = []
+// 		for(i=0; i<totalInList; i++) {
+// 			var newPerson = { 
+// 				name: prompt("Name number " + (i + 1) + "?"),
+// 				tel: prompt("Enter number " + (i + 1) + "'s phone number:"),
+// 				street: prompt("Enter number " + (i + 1) + "'s street name:")
+// 			}
+// 			newArray.push(newPerson);
+// 		}
+// 		return newArray;
+// 	};
+
+// 	//Add people to the victims list
+// 	var numberOfVictims = parseInt(prompt("How many victims are there to help?"));
+// 	var victims = addPeopleToList(numberOfVictims);
+	
+// 	//Add people to the volunteers list
+// 	var numberOfVolunteers = +prompt("How many volunteers are there?");
+// 	var volunteers = addPeopleToList(numberOfVolunteers);
+
+// 	//Create list of victims' names
+// 	var victimsList = "";
+// 	for(i=0; i < victims.length; i++) {
+// 		victimsList = victimsList + "\n\t " + (i + 1) + ". " + victims[i]['name'];
+// 	}
+
+// 	//Creat list of volunteers' names
+// 	var volunteersList = "";
+// 	for(i=0; i < victims.length; i++) {
+// 		volunteersList = volunteersList + "\n\t " + (i + 1) + ". " + volunteers[i]['name'];
+// 	}
+
+// 	alert("Thanks for helping us start Disaster Relief.\n\nThere are " 
+// 		+ numberOfVictims + " people who need help. \nAnd, " + numberOfVolunteers + 
+// 		" volunteers who can help.\n\nNeed Help:" + victimsList + 
+// 		"\n\nOffering Help:" + volunteersList);
+
+// };
+
 function startDisasterRelief() {
 	// Adds people to list - takes in the list name and total to be added
 	// and outputs the updated list with each person as an object
-	var addPeopleToList = function(totalInList) {
+	var addPeopleToList = function(morePeople) {
 		var newArray = []
-		for(i=0; i<totalInList; i++) {
+		while(morePeople) {
 			var newPerson = { 
-				name: prompt("Name number " + (i + 1) + "?"),
-				tel: prompt("Enter number " + (i + 1) + "'s phone number:"),
-				street: prompt("Enter number " + (i + 1) + "'s street name:")
+				name: prompt("Enter a name:"),
+				tel: prompt("Enter their phone number:"),
+				street: prompt("Enter their street name:")
 			}
 			newArray.push(newPerson);
+			morePeople = confirm("Click OK to enter another person's information and Cancel to stop.")
 		}
 		return newArray;
 	};
 
 	//Add people to the victims list
-	var numberOfVictims = parseInt(prompt("How many victims are there to help?"));
-	var victims = addPeopleToList(numberOfVictims);
+	var victimsToEnter = confirm("Click OK if you have victims to enter and Cancel if don't have any to enter.")
+	var victims = addPeopleToList(victimsToEnter);
 	
 	//Add people to the volunteers list
-	var numberOfVolunteers = +prompt("How many volunteers are there?");
-	var volunteers = addPeopleToList(numberOfVolunteers);
+	var volunteersToEnter = confirm("Click OK if you have volunteers to enter and Cancel if don't have any to enter.")
+	var volunteers = addPeopleToList(volunteersToEnter);
 
 	//Create list of victims' names
 	var victimsList = "";
@@ -35,9 +79,9 @@ function startDisasterRelief() {
 	}
 
 	alert("Thanks for helping us start Disaster Relief.\n\nThere are " 
-		+ numberOfVictims + " people who need help. \nAnd, " + numberOfVolunteers + 
+		+ victims.length + " people who need help. \nAnd, " + volunteers.length + 
 		" volunteers who can help.\n\nNeed Help:" + victimsList + 
 		"\n\nOffering Help:" + volunteersList);
-
 };
+
 startDisasterRelief();
